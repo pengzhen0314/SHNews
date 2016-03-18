ecece249bb3e9dd863f8e0550c13b6050848f2c2
彭臻 <pengzhen_0314@126.com>
2016-03-19 01:46:18 +0800

    初始化项目

---
+++ 新闻/新闻/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  新闻
+//
+//  Created by pengzhen on 16/3/19.
+//  Copyright © 2016年 pengzhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 新闻/新闻/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  新闻
+//
+//  Created by pengzhen on 16/3/19.
+//  Copyright © 2016年 pengzhen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
